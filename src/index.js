dd71a9b27631850662a99abb4f56d6361367137c
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-18 20:21:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
