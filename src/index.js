7e7145296bbe0f3ed1980a04e5bddbccdc725d8e
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-19 23:18:55 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       history: history.concat([{
-        squares
+        squares: squares
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext
@@ -105,7 +105,7 @@ class Game extends React.Component {
         <div className='game-board'>
           <Board
             squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+            onClick={i => this.handleClick(i)}
           />
         </div>
         <div className='game-info'>
