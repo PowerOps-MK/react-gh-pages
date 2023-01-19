f9321a62e1676bf33badf598b06f6fba7e18dc67
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-19 21:48:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,28 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 class Square extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      value: null
-    }
-  }
-
   render () {
     return (
-      <button
-        className='square'
-        onClick={() => this.setState({ value: 'X' })}
-      >
-        {this.state.value}
+      <button className="square">
+        {this.props.value}
       </button>
-    )
+    );
   }
 }
 
 class Board extends React.Component {
   renderSquare (i) {
-    return <Square value={i} />
+    return <Square value={i} />;
   }
 
   render () {
