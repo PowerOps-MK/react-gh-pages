3f7909c6ab8d6b8db789d149ffec6a310e0cd3e0
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-25 19:51:38 +0100

    Update index.js

--- Buttons/src/index.js
+++ Buttons/src/index.js
@@ -80,6 +80,7 @@ export default function MyApp () {
     <div>
       <h1>Welcome to my app</h1>
       <MyButton />
+      <MyButton />
       <AboutPage />
       <Profile />
       <ShoppingList />
