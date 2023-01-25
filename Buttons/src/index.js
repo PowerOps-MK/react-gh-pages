aefae8a8a0b2aa5daaca8d70674038e017da7743
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-25 18:41:51 +0100

    Update index.js

--- Buttons/src/index.js
+++ Buttons/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-function MyButton() {
+function MyButton () {
   return (
     <button>I'm a button</button>
-  );
+  )
 }
 
-export default function MyApp() {
+export default function MyApp () {
   return (
     <div>
       <h1>Welcome to my app</h1>
       <MyButton />
     </div>
-  );
+  )
 }
