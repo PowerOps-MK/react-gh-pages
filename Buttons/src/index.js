5e9bc20192ff408ec704b5e0fd5d078ee8eb2f50
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-25 18:31:49 +0100

    Update index.js

--- Buttons/src/index.js
+++ Buttons/src/index.js
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-function Square (props) {
+function MyButton() {
   return (
-    <button className='square' onClick={props.onClick}>
-      {props.value}
-    </button>
-  )
+    <button>I'm a button</button>
+  );
 }
