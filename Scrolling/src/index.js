6e84deb4722289ddcdbd7af2edfb9ad799b74a0c
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-26 21:30:55 +0100

    Update index.js

--- Scrolling/src/index.js
+++ Scrolling/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { animated } from '@react-spring/web'
 
 export default function MyApp () {
   return (
     <div>
-    
+      <h1> Hello </h1>
     </div>
   )
 }
