a0eb01284ba9b0ae3d8b19e3c7c1e6eb6f23df1a
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-26 23:16:38 +0100

    Update index.js

--- Scrolling/src/index.js
+++ Scrolling/src/index.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { animated } from '@react-spring/web'
+import { useSpring, animated } from '@react-spring/web'
 import './index.css'
 
 export default function MyComponent () {
+  const springs = useSpring({
+    from: { x: 0 },
+    to: { x: 100 }
+  })
+
   return (
-    <animated.div
-      style={{
-        width: 80,
-        height: 80,
-        background: '#ff6d6d',
-        borderRadius: 8
-      }}
-    />
+    <animated.div />
   )
 }
 
