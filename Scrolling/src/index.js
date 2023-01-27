ba6a27fa01a72a60157e0f49fad4528578ed0e2b
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-27 21:06:01 +0100

    Update index.js

--- Scrolling/src/index.js
+++ Scrolling/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom'
 import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
 import './index.css'
 
-const url = (name: string, wrap = false) =>
-  `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 
 export default function MyApp () {
-  const parallax = useRef<IParallax>(null!),
+  const parallax = useRef<IParallax>(null!)
   return (
     <div style={{ width: '100%', height: '100%', background: '#253237' }}>
 
