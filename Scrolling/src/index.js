6e6059492b8f0d33fc7a0c607e2f6c6668c23b4e
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-27 21:02:59 +0100

    Update index.js

--- Scrolling/src/index.js
+++ Scrolling/src/index.js
@@ -7,7 +7,7 @@ const url = (name: string, wrap = false) =>
   `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 
 export default function MyApp () {
-  const parallax = useRef<IParallax>(null!)
+  const parallax = useRef<IParallax>(null!),
   return (
     <div style={{ width: '100%', height: '100%', background: '#253237' }}>
 
