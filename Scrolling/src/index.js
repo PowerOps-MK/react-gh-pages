cef5045fa73e4653d85d940610fddf4d8e87549e
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-27 22:20:14 +0100

    Update index.js

--- Scrolling/src/index.js
+++ Scrolling/src/index.js
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react'
 import ReactDOM from 'react-dom'
-import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { IParallax } from '@react-spring/parallax'
 
 const url = (name: string, wrap = false) =>
   `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
