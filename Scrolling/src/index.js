a361731125f4eee802865d63049d93f6a85705e7
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-27 13:37:51 +0100

    Update index.js

--- Scrolling/src/index.js
+++ Scrolling/src/index.js
@@ -4,10 +4,20 @@ import { useSpring, animated } from '@react-spring/web'
 import './index.css'
 
 export default function MyComponent () {
-  const springs = useSpring({
+  const [springs, api] = useSpring(() => ({
     from: { x: 0 },
-    to: { x: 100 }
-  })
+  }))
+  
+  const handleClick = () => {
+    api.start({
+      from: {
+        x: 0,
+      },
+      to: {
+        x: 100,
+      },
+    })
+  }
 
   return (
     <animated.div
