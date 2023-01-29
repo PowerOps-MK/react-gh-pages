0263eca8f679d1a3592eadfe1412f5ba5786b601
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-29 18:11:10 +0100

    Update index.js

--- Scrolling/src/index.js
+++ Scrolling/src/index.js
@@ -1,27 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+//import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
-const url = (name: string, wrap = false) =>
-  `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
-
-export default function MyApp () {  
+export default function MyApp () {
   return (
-    <div style={{ width: '100%', height: '100%', background: '#253237' }}>
-      <Parallax pages={3}>
-        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
-        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
-        
-        <ParallaxLayer
-          offset={0}
-          speed={0}
-          factor={3}
-          style={{
-            backgroundImage: url('stars', true),
-            backgroundSize: 'cover'
-          }}
-        />
-      </Parallax>
+    <div>
+      <h1>hello</>
     </div>
   )
 }
