07740aba719328635a760b890f86d25aef12053d
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-29 21:08:45 +0100

    Update index.js

--- Parallax/src/index.js
+++ Parallax/src/index.js
@@ -29,24 +29,6 @@ export default function MyApp () {
         <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
           <p className='scroll-text'>Scroll down</p>
         </ParallaxLayer>
-
-        <ParallaxLayer sticky={{ start: 1, end: 2 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
-          <div className='card sticky'>
-            <p>I'm a sticky layer</p>
-          </div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
-          <div className='card parallax green'>
-            <p>I'm not</p>
-          </div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={2} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
-          <div className='card parallax blue'>
-            <p>Neither am I</p>
-          </div>
-        </ParallaxLayer>
       </Parallax>
     </div>
   )
