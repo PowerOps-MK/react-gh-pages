ce7e5a9e28f9ef4bb057f165ddacad04e9e0dca3
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-29 20:25:27 +0100

    Update index.js

--- Scrolling/src/index.js
+++ Scrolling/src/index.js
@@ -5,11 +5,23 @@ import './index.css'
 
 export default function MyApp () {
   const alignCenter = { display: 'flex', alignItems: 'center' }
+  const url = (name: string, wrap = false) =>
+  `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 
   return (
     <div>
       <div className='background' />
       <Parallax pages={3}>
+        <ParallaxLayer
+          offset={0}
+          speed={0}
+          factor={3}
+          style={{
+            backgroundImage: url('stars', true),
+            backgroundSize: 'cover',
+          }}
+        />
+
         <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
           <p className='scroll-text'>Scroll down</p>
         </ParallaxLayer>
