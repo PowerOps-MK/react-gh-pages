dd20035199361e57f2a3d292e9aa62f5dea35c1b
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-29 23:51:29 +0100

    Update index.js

--- Parallax/src/index.js
+++ Parallax/src/index.js
@@ -58,7 +58,7 @@ export default function MyApp () {
           <img src={url('clients-main')} alt='clients' style={{ width: '40%' }} />
         </ParallaxLayer>
 
-        <ParallaxLayer sticky={{ start: 1.5, end: 2.5 }} style={{ opacity: 0.6 }}>
+        <ParallaxLayer sticky={{ start: 1.5, end: 2 }} style={{ opacity: 0.5 }}>
           <img src={url('earth')} alt='earth' style={{ width: '7%', marginLeft: '15%' }} />
         </ParallaxLayer>
       </Parallax>
