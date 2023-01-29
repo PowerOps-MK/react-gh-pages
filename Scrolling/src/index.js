c326f64e2274fb78b503aab5c5ea822c9bbb94ee
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-29 18:49:40 +0100

    Update index.js

--- Scrolling/src/index.js
+++ Scrolling/src/index.js
@@ -11,8 +11,15 @@ export default function MyApp () {
       <div className='background' />
       <Parallax pages={3}>
         <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
-          <p className="scroll-text">Scroll down</p>
+          <p className='scroll-text'>Scroll down</p>
         </ParallaxLayer>
+
+        <ParallaxLayer sticky={{ start: 1, end: 3 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
+          <div className='card'>
+            <p>I'm a sticky layer</p>
+          </div>
+        </ParallaxLayer>
+
       </Parallax>
     </div>
   )
