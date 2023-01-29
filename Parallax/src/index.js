833f8781f067941dfa59acda146c7d0c74a80cb7
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-29 21:51:42 +0100

    Update index.js

--- Parallax/src/index.js
+++ Parallax/src/index.js
@@ -24,26 +24,6 @@ export default function MyApp () {
         <animated.div style={props}>Hello World</animated.div>
       ))}
 
-      <div className='background' />
-      <Parallax pages={3}>
-        <ParallaxLayer
-          offset={0}
-          speed={0}
-          factor={3}
-          style={{
-            backgroundImage: url('stars', true),
-            backgroundSize: 'cover'
-          }}
-        />
-
-        <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
-          <p className='scroll-text'>Scroll down</p>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
-          <img src={url('satellite4')} alt='satellite' style={{ width: '15%', marginLeft: '70%' }} />
-        </ParallaxLayer>
-      </Parallax>
     </div>
   )
 }
