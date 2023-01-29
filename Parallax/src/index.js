66c9130c7997def38a492f81957f0f4428b24ce8
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-29 23:06:40 +0100

    Update index.js

--- Parallax/src/index.js
+++ Parallax/src/index.js
@@ -9,9 +9,9 @@ export default function MyApp () {
   const url = (name, wrap = false) =>
   `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
   const props = useSpring({
-    from: { opacity: 0, color: '#f8f8ff', fontSize: '1.5em' },
+    from: { opacity: 0, color: '#f8f8ff', fontSize: '2em' },
     to: { opacity: 1, color: '#000', fontSize: '3em' },
-    config: { duration: 2500 },
+    config: { duration: 2000 },
     delay: 500
   })
 
