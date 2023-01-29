9f8ca987d78881b52d451e776aa15e88c94aaabf
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-29 21:46:01 +0100

    Update index.js

--- Parallax/src/index.js
+++ Parallax/src/index.js
@@ -2,14 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import './index.css'
+import { useTrail, animated } from '@react-spring/web'
 
 export default function MyApp () {
   const alignCenter = { display: 'flex', alignItems: 'center' }
   const url = (name, wrap = false) =>
   `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 
+  const [trails, api] = useTrail(
+    2,
+    () => ({
+      from: { opacity: 0 },
+      to: { opacity: 1 },
+    }),
+    []
+  )
+
   return (
     <div>
+      {trails.map(props => (
+        <animated.div style={props}>Hello World</animated.div>
+      ))}
+
       <div className='background' />
       <Parallax pages={3}>
         <ParallaxLayer
