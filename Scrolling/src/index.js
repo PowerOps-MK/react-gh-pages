3a89c3aa081bf05e48af09e66b56becec28e6597
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-29 20:32:11 +0100

    Update index.js

--- Scrolling/src/index.js
+++ Scrolling/src/index.js
@@ -22,6 +22,10 @@ export default function MyApp () {
           }}
         />
 
+       <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
+          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
+        </ParallaxLayer>
+
         <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
           <p className='scroll-text'>Scroll down</p>
         </ParallaxLayer>
