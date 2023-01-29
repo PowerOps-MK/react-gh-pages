9c8e6ce85f121cf84b0857836a498ac51648aba2
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-29 22:56:47 +0100

    Update index.js

--- Parallax/src/index.js
+++ Parallax/src/index.js
@@ -35,8 +35,9 @@ export default function MyApp () {
 
         <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
           <img src={url('satellite4')} alt='satellite' style={{ width: '15%', marginLeft: '70%' }} />
+          <img src={url('satellite4')} alt='satellite' style={{ width: '5%', marginLeft: '20%' }} />
         </ParallaxLayer>
-        
+
         <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
           <img src={url('cloud')} alt='cloud' style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
           <img src={url('cloud')} alt='cloud' style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
