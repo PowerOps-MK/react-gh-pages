71886e09bddbc3315f4f13bf351ff2889e172b6e
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 19:20:10 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -4,9 +4,13 @@ import './index.css'
 
 export default function MyApp () {
   return (
-    <div className='bg-gray-200 py-4 dark:bg-gray-800 shadow-sm rounded-lg'>
-      <div className='mx-auto'>
-        Hello World
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+      <div class="shrink-0">
+        <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+      </div>
+      <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-slate-500">You have a new message!</p>
       </div>
     </div>
   )
