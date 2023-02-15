164fa7f6dfc428d4e06c1442a22cc724442df755
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 19:01:17 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-export default function MyApp () {  
+export default function MyApp () {
   return (
-    <div className="bg-gray-200">
-      <div className="md:w-1/2 mx-auto">
-        Hello
+    <div className='bg-gray-200'>
+      <div className='mx-auto'>
+        Hello World
       </div>
     </div>
   )
