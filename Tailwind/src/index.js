f92a96ae39eab541ae0089d41e0f8a7aa4c16349
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 18:25:49 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import './index.css'
 
 export default function MyApp () {
@@ -9,29 +8,6 @@ export default function MyApp () {
   return (
     <div>
       <div className='background' />
-      <Parallax pages={3}>
-        <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
-          <p className='scroll-text'>Scroll down</p>
-        </ParallaxLayer>
-
-        <ParallaxLayer sticky={{ start: 1, end: 2 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
-          <div className='card sticky'>
-            <p>I'm a sticky layer</p>
-          </div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
-          <div className='card parallax green'>
-            <p>I'm not</p>
-          </div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={2} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
-          <div className='card parallax blue'>
-            <p>Neither am I</p>
-          </div>
-        </ParallaxLayer>
-      </Parallax>
     </div>
   )
 }
