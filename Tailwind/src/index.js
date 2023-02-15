6429da056f04f53b47177860e1da6f1d69e9227f
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 23:29:30 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -5,7 +5,7 @@ import './index.css'
 export default function MyApp () {
   return (
     <body class='min-h-screen bg-gradient-to-b from-indigo-800 to-black'>
-      <div class='flex flex-col lg:flex-row space-y-8 space-x-6 p-6'>
+      <div class='flex flex-col lg:flex-row space-y-8 lg:space-x-6 p-6'>
         <div class='bg-indigo-700 shadow-xl rounded flex items-center space-x-4 lg:w-1/2'>
           <div class='py-4 pl-4'>
             <img class='h-16 w-16' src='https://www.freeiconspng.com/uploads/iphone-message-icon-png-22.png' alt='ChitChat Logo' />
