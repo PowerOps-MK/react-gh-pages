bbe3284b2615598e207e42ab1f98559c488ea1da
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 18:51:38 +0100

    Update tailwind.config.js

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: true,
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "media",
   theme: {
     extend: {},
   },
