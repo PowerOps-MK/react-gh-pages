691b884969c3597bf05f9a275022d2d8a9d38d31
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 21:43:53 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -5,10 +5,10 @@ import './index.css'
 export default function MyApp () {
   return (
     <body class='min-h-screen bg-gradient-to-b from-indigo-800 to-black'>
-      <div>
-        <div class='p-8 bg-indigo-700 space-y-4'>
+      <div flex flex-col space-y-4>
+        <div class='p-8 bg-indigo-700'>
         </div>
-        <div class='p-8 bg-indigo-700 space-y-4'>
+        <div class='p-8 bg-indigo-700'>
         </div>
       </div>
     </body>
