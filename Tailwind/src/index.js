bb6983e8e0664eace650319cf0a88c51fed18bc3
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 22:37:31 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -6,7 +6,7 @@ export default function MyApp () {
   return (
     <body class='min-h-screen bg-gradient-to-b from-indigo-800 to-black'>
       <div class='flex flex-col space-y-6 p-6'>
-        <div class='bg-indigo-700 rounded'>
+        <div class='bg-indigo-700 rounded flex items-center space-x-4'>
           <div class='shrink-0'>
             <img class='h-12 w-12 rounded' src='https://play-lh.googleusercontent.com/ZSqfn8W1VzWp80p9aI4RFBaou0z0O5YN5sHEQHOKzK5spwiitvaj5Jq8HxnqxC6JLQ=w480-h960' alt='ChitChat Logo' />
           </div>
@@ -16,9 +16,13 @@ export default function MyApp () {
           </div>
         </div>
 
-        <div class='bg-indigo-700 rounded'>
+        <div class='bg-indigo-700 rounded flex items-center space-x-4'>
           <div class='shrink-0'>
             <img class='h-12 w-12 rounded' src='https://play-lh.googleusercontent.com/ZSqfn8W1VzWp80p9aI4RFBaou0z0O5YN5sHEQHOKzK5spwiitvaj5Jq8HxnqxC6JLQ=w480-h960' alt='ChitChat Logo' />
+          </div>
+          <div>
+            <div class='text-xl font-medium text-black'>ChitChat</div>
+            <p class='text-slate-500'>You have a new message!</p>
           </div>
         </div>
       </div>
