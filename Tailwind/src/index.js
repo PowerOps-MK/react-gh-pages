e4c9487eeb49ab3e8e62e8c5cef476ea6e786a1d
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 22:53:35 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -7,21 +7,21 @@ export default function MyApp () {
     <body class='min-h-screen bg-gradient-to-b from-indigo-800 to-black'>
       <div class='flex flex-col space-y-6 p-6'>
         <div class='bg-indigo-700 rounded flex items-center space-x-4'>
-          <div class='shrink-0'>
+          <div class=''>
             <img class='h-16 w-16' src='https://www.freeiconspng.com/uploads/iphone-message-icon-png-22.png' alt='ChitChat Logo' />
           </div>
           <div>
-            <div class='text-xl font-medium text-black'>ChitChat</div>
+            <div class='text-xl font-medium text-black'>Chat</div>
             <p class='text-slate-500'>You have a new message!</p>
           </div>
         </div>
 
         <div class='bg-indigo-700 rounded flex items-center space-x-4'>
-          <div class='shrink-0'>
+          <div class='shrink-0 p-4'>
             <img class='h-16 w-16' src='https://www.freeiconspng.com/uploads/iphone-message-icon-png-22.png' alt='ChitChat Logo' />
           </div>
           <div>
-            <div class='text-xl font-medium text-black'>ChitChat</div>
+            <div class='text-xl font-medium text-black'>Chat</div>
             <p class='text-slate-500'>You have a new message!</p>
           </div>
         </div>
