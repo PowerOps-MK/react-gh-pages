a6e5d5cc587e3d3b4ced95c73582ecab1b0446eb
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 22:23:51 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -8,13 +8,13 @@ export default function MyApp () {
       <div class='flex flex-col space-y-6 p-6'>
         <div class='bg-indigo-700 rounded'>
           <div class='shrink-0'>
-            <img class='h-12 w-12' src='https://play-lh.googleusercontent.com/ZSqfn8W1VzWp80p9aI4RFBaou0z0O5YN5sHEQHOKzK5spwiitvaj5Jq8HxnqxC6JLQ=w480-h960' alt='ChitChat Logo' />
+            <img class='h-12 w-12 rounded' src='https://play-lh.googleusercontent.com/ZSqfn8W1VzWp80p9aI4RFBaou0z0O5YN5sHEQHOKzK5spwiitvaj5Jq8HxnqxC6JLQ=w480-h960' alt='ChitChat Logo' />
           </div>
         </div>
 
         <div class='bg-indigo-700 rounded'>
           <div class='shrink-0'>
-            <img class='h-12 w-12' src='https://play-lh.googleusercontent.com/ZSqfn8W1VzWp80p9aI4RFBaou0z0O5YN5sHEQHOKzK5spwiitvaj5Jq8HxnqxC6JLQ=w480-h960' alt='ChitChat Logo' />
+            <img class='h-12 w-12 rounded' src='https://play-lh.googleusercontent.com/ZSqfn8W1VzWp80p9aI4RFBaou0z0O5YN5sHEQHOKzK5spwiitvaj5Jq8HxnqxC6JLQ=w480-h960' alt='ChitChat Logo' />
           </div>
         </div>
       </div>
