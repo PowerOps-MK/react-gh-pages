9d3dfa0be9fdf3da828d0bbc56815cbb7f813d2f
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 21:18:46 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -5,7 +5,8 @@ import './index.css'
 export default function MyApp () {
   return (
     <body class='min-h-screen dark:bg-gradient-to-b from-indigo-700 to-black'>
-      Hello world 🌎
+      <div class='p-6 max-w-sm mx-auto rounded-xl shadow-lg flex items-center space-x-4'>
+      </div>
     </body>
   )
 }
