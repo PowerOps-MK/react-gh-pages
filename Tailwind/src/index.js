3ce01aa595f5d5ead64117b60e3f9391f3a087a6
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 19:06:20 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 
 export default function MyApp () {
   return (
-    <div className='bg-gray-200 py-4 dark:bg-gray-800'>
+    <div className='bg-gray-200 py-4 dark:bg-gray-800 shadow-sm rounded-lg'>
       <div className='mx-auto'>
         Hello World
       </div>
