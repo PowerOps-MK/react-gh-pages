6d2b2038dc23e70c0035f872c80a02d8ddc0a7c3
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 22:24:39 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -10,6 +10,10 @@ export default function MyApp () {
           <div class='shrink-0'>
             <img class='h-12 w-12 rounded' src='https://play-lh.googleusercontent.com/ZSqfn8W1VzWp80p9aI4RFBaou0z0O5YN5sHEQHOKzK5spwiitvaj5Jq8HxnqxC6JLQ=w480-h960' alt='ChitChat Logo' />
           </div>
+          <div>
+            <div class='text-xl font-medium text-black'>ChitChat</div>
+            <p class='text-slate-500'>You have a new message!</p>
+          </div>
         </div>
 
         <div class='bg-indigo-700 rounded'>
