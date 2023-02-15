cf900fc6f8b1e78d7ca441866dc725a12215a197
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-15 20:08:29 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 
 export default function MyApp () {
   return (
-    <div class='bg-indigo-500'>
+    <div class='bg-cyan-700'>
       <div class='p-6 max-w-sm mx-auto rounded-xl shadow-lg flex items-center space-x-4'>
         <div class='shrink-0'>
           <img class='h-12 w-12' src='https://play-lh.googleusercontent.com/ZSqfn8W1VzWp80p9aI4RFBaou0z0O5YN5sHEQHOKzK5spwiitvaj5Jq8HxnqxC6JLQ=w480-h960' alt='ChitChat Logo' />
