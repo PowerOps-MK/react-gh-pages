9e8021b011fb354e4468751de9bb84f8683d7af5
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-06 18:25:12 +0100

    Update index.js

--- Tailwind/src/index.js
+++ Tailwind/src/index.js
@@ -24,11 +24,6 @@ export default function MyApp () {
             <div class='text-xl font-medium text-black'>Chat</div>
             <p class='text-slate-500'>You have a new message!</p>
           </div>
-          <div class='justify-end pr-4 flex items-end'>
-            <button id='dropdownButton' data-dropdown-toggle='dropdown' class='inline-block text-gray-400 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-700 rounded-lg text-sm p-1.5' type='button'>
-              <svg class='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path d='M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z'></path></svg>
-            </button>
-          </div>
         </div>
       </div>
     </body>
